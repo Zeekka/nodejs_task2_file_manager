b68039f8f1a94591ea68e52458659f4673f23ff8
Yauheni Udavihin <e.udavihin@itransition.com>
2022-06-06 14:07:04 +0300

    Implement rn command

---
+++ src/app/commands/rn.js
@@ -0,0 +1,23 @@
+import {workerData, parentPort} from 'node:worker_threads';
+import {assemblePathForSingleArg} from '../helpers/composers.js';
+import {rename} from 'node:fs/promises';
+import path from 'node:path';
+
+export const rn = async () => {
+    if (workerData.args.length !== 2) {
+        throw new Error('Operation failed');
+    }
+
+    const newFileName = workerData.args.splice(-1).pop();
+    const file = await assemblePathForSingleArg(workerData.currentDir, workerData.args);
+    const renameResult = await rename(file, path.join(path.dirname(file), newFileName)).catch(err => err);
+
+    if (renameResult instanceof Error) {
+        throw new Error('Operation failed');
+    }
+
+    parentPort.postMessage({});
+    return 0;
+};
+
+rn();
