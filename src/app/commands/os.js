d6a16cce3bbbe92a3070feff5ef88f7e9092cbbe
Yauheni Udavihin <e.udavihin@itransition.com>
2022-06-06 15:23:59 +0300

    Implement os command

---
+++ src/app/commands/os.js
@@ -0,0 +1,28 @@
+import {workerData, parentPort} from 'node:worker_threads';
+import * as nos from 'node:os';
+
+export const os = async () => {
+    const argRegExp = /^--/;
+    if (workerData.args.length !== 1 || !argRegExp.test(workerData.args[0])) {
+        throw new Error('Operation failed');
+    }
+
+    const arg = workerData.args.pop();
+    let requestedInfo;
+
+    switch (arg) {
+        case '--EOL' : requestedInfo = nos.EOL.replace('\n', '\\n').replace('\r', '\\r'); break;
+        case '--cpus': requestedInfo = nos.cpus(); break;
+        case '--homedir': requestedInfo = nos.homedir(); break;
+        case '--username': requestedInfo = nos.hostname(); break;
+        case '--architecture': requestedInfo = nos.arch(); break;
+        default: throw new Error('Operation failed');
+    }
+
+    parentPort.postMessage({
+        commandResult: requestedInfo,
+    });
+    return 0;
+};
+
+os();
