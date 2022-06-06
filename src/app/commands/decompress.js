dd42d21e1d287518452cd43fb270fb2c7d84ee79
Yauheni Udavihin <e.udavihin@itransition.com>
2022-06-06 17:28:03 +0300

    Implement decompress function

---
+++ src/app/commands/decompress.js
@@ -0,0 +1,27 @@
+import {workerData, parentPort} from 'node:worker_threads';
+import {assemblePathForTwoArgs} from '../helpers/composers.js';
+import {open} from 'node:fs/promises';
+import fs from 'node:fs';
+import zlib from 'node:zlib';
+
+export const decompress = async () => {
+    const commandContext = await assemblePathForTwoArgs(workerData.currentDir, workerData.args);
+    const fd = await open(commandContext.src).catch(err => err);
+
+    if (fd instanceof Error) {
+        throw new Error('Operation failed');
+    }
+
+    const readStream = fd.createReadStream();
+    const gZip = zlib.createBrotliDecompress();
+    const writeStream = fs.createWriteStream(commandContext.dest);
+
+    readStream
+        .pipe(gZip)
+        .pipe(writeStream);
+
+    parentPort.postMessage({});
+    return 0;
+};
+
+decompress();
